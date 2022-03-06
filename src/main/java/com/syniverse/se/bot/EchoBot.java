dbd447d881399fac8fc021a97444365b1a8c2409
Rakesh Kaatkar <rakesh.kaatkar@syniverse.com>
2022-03-06 20:55:53 +0530

    Removed log

--- src/main/java/com/syniverse/se/bot/EchoBot.java
+++ src/main/java/com/syniverse/se/bot/EchoBot.java
@@ -49,8 +49,8 @@ public class EchoBot extends ActivityHandler {
             //echoMsg = "Echo: " + new ObjectMapper().writeValueAsString(turnContext.getActivity().getAttachments().get(0));
             //LOGGER.info(">>> Replying with message: {}",echoMsg + turnContext.getActivity().getType());
             return turnContext.sendActivity(
-                    //MessageFactory.text(turnContext.getActivity().getAttachments().get(0).getName())
-                    MessageFactory.attachment(turnContext.getActivity().getAttachments())
+                    MessageFactory.text(turnContext.getActivity().getAttachments().get(0).getName())
+                    //MessageFactory.attachment(turnContext.getActivity().getAttachments())
             ).thenApply(sendResult -> null);
             
         } else if (turnContext.getActivity().getText() != null) {
